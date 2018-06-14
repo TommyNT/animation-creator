04263711d5f8baf22c8749687fe3cac546012a9b
Tuan Duong Nguyen <tommynt@outlook.com>
2018-06-14 17:25:48 +0000

    name_generator layout fix

--- media/name_generator.py
+++ media/name_generator.py
@@ -1,7 +1,7 @@
 import uuid
-
 def generate_name(filename):
-	return "{}/{}/{}".format(
-		"uploads", 
-		uuid.uuid4(),
-		filename)
+  return "{}/{}/{}".format(
+    "uploads",
+    uuid.uuid4(),
+    filename
+  )
